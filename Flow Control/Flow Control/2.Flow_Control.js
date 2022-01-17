91ac8d813c20de00f9c28b798a62892b4f779238
RIPUSUDAN CHOUREY <53904872+RIPUSUDAN-CHOUREY@users.noreply.github.com>
2022-01-17 12:36:04 +0530

    Add files via upload

---
+++ Flow Control/Flow Control/2.Flow_Control.js
@@ -0,0 +1,21 @@
+function get(){
+    return "Morning Sunshine Beautiful";
+}
+console.log(get());
+
+//with pararmeter
+function greet(name){
+    return "hello " +name 
+}
+let name = ("Ripusudan")
+console.log(greet(name))
+
+
+// adiition of two number in function
+
+function myfunction(p1,p2){
+    
+    return (p1+p2);
+    
+}
+console.log(myfunction(2,3));
